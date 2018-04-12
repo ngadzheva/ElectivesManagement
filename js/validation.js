c079a9c2fa8873ffdf1ef7eaadb2548fd4b9c3c8
ngadzheva <nmgadzheva@gmail.com>
2018-04-13 00:34:01 +0300

    Login works. No validation

--- js/validation.js
+++ js/validation.js
@@ -1,31 +1,26 @@
 function validate(){
-    var user = document.forms['login']['user'].value;
-    var password = document.forms['login']['password'].value;
+    var user = document.getElementsByName('user')[0].value;
+    var password = document.getElementsByName('password')[0].value;
 
     if(user === 'admin' && password === 'admin'){
-        var action = document.forms['login'].getAttribute('action');
-        action.value = 'admin.html';
+        var action = document.getElementsByName('login')[0].setAttribute('action', 'admin.html');
 
         return true;
     } else if(user === 'lecturer' && password === 'lecturer'){
-        var action = document.forms['login'].getAttribute('action');
-        action.value = 'lecturer.html';
+        var action = document.getElementsByName('login')[0].setAttribute('action', 'lecturer.html');
 
         return true;
     } else if(user === 'student' && password === 'student'){
-        var action = document.forms['login'].getAttribute('action');
-        action.value = 'student.html';
+        var action = document.getElementsByName('login')[0].setAttribute('action', 'student.html');
 
         return true;
     } else {
         var error = document.createElement('label');
-        var att = document.createAttribute('id');
-        att.value = 'error';
-        error.appendChild(att);
+        error.setAttribute('id', 'error');
         var value = document.createTextNode('Невалидно име или парола!');
         error.appendChild(value);
         var userNameLabel = document.getElementsByClassName('login')[1];
-        document.forms['login'].insertBefore(userNameLabel, error);
+        document.getElementsByName('login')[0].insertBefore(userNameLabel, error);
 
         return false;
     }
