15e7fbeb6e5ffb83677ca186892148de6b094fd2
KDimova <38068970+KDimova@users.noreply.github.com>
2018-06-07 13:53:38 +0300

    Create student.php

---
+++ php/student.php
@@ -0,0 +1,43 @@
+<?php
+
+require "User.php"
+
+class Student extends User{
+	
+	require_once( "database.php");
+	private $fn;
+	private $names;
+	private $year;
+	private $bachelorProgram;
+		
+	public function __construct($fn, $names, $year, $bachelorProgram){
+		$this->database = new DataBase("localhost", "uxProj", "root", "");
+		$this->fn= $fn;
+		$this->names=$names;
+		$this->year=$year;
+		$this->bachelorProgram=$bachelorProgram;
+	}
+	
+	public function get_fn(){
+		return $this->fn;
+	}
+	
+	public function get_names(){
+		return $this->names;
+	}
+	
+	public function get_year(){
+		return $this->year;
+	}
+	
+	public function get_bachelorProgram(){
+		return $this->bachelorProgram;
+	}
+	
+	public function set_year($year){
+		$this->year = $year;
+		$sql = "UPDATE student SET year = $year  WHERE year = $year ";
+        $query = $this->database->executeQuery($sql, "Failed updating year!");
+	}
+	
+?>
